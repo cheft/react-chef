2a7f5ccb3dc8b1dc835c4ce7e5abd2fc3765b8a6
陈海峰 <cn.cheft@gmail.com>
2017-09-27 15:27:29 +0800

    完善mobx严格模式，创建 ssr[wip] 项目

--- react-native-mobx/src/app.js
+++ react-native-mobx/src/app.js
@@ -3,6 +3,9 @@ import { View, StatusBar } from 'react-native';
 import { StackNavigator } from 'react-navigation';
 import List from './pages/list';
 import Detail from './pages/detail';
+import { useStrict } from 'mobx';
+
+useStrict(true);
 
 const AppNavigator = StackNavigator({
   List: { screen: List },
